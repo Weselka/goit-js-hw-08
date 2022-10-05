51033d774ba0244c0264ace9bd5bc8931e93b552
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-10-05 18:46:05 +0300

    adds template

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,3 +6,26 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(SimpleLightbox);
 console.log(galleryItems);
+
+let items = galleryItems;
+
+const galleryTemplate = ({ preview, original, description }) =>
+  `<a class="gallery__item" href="${preview}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+
+const refs = {
+  containerGalleryItems: document.querySelector('.gallery'),
+};
+
+const render = () => {
+  const imgEl = items.map(galleryTemplate).join('');
+  refs.containerGalleryItems.insertAdjacentHTML('beforeend', imgEl);
+};
+
+render();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
