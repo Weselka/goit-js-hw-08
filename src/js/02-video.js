bb0d469ab029814c0cfb7cdaf18e918b3a10bb95
Weselka <weselyna.shvets@gmail.com>
2022-10-08 19:50:41 +0300

    task2 update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,34 +1,13 @@
-
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const refs = {
+  iframe: document.querySelector('#vimeo-player'),
+};
+const playerEl = new Player(refs.iframe);
+const onPlay = function(data) {
+   localStorage.setItem('videoplayer-current-time', data.seconds);
+ };
 
-
-player.on('play', function () {
-  console.log('played the video!');
-});
-const iframe = document.querySelector('#vimeo-player');
-console.log(iframe);
-// const player = new Vimeo.Player(iframe);
-// console.log(player);
-
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
-
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
-
-// const onPlay = function (data) {
-//   // data is an object containing properties specific to that event
-// };
-
-// player.on('play', onPlay);
-
-// player.on('eventName', function (data) {
-//   // data is an object containing properties specific to that event
-// });
-
-// var callback = function () {};
-
-// player.off('eventName', callback);
+playerEl.on('timeupdate', throttle(onPlay, 1000));
+playerEl.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
