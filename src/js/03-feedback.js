db36bb05961f05d792aec71a6a09b8cadc6af8bc
Weselka <weselyna.shvets@gmail.com>
2022-10-09 18:02:05 +0300

    task3 update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,10 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
 const refs = {
   form: document.querySelector('.feedback-form'),
   textarea: document.querySelector('.feedback-form textarea'),
+  input: document.querySelector('.feedback-form input'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
@@ -14,34 +14,39 @@ populateForm();
 
 function onFormSubmit(event) {
   event.preventDefault();
-  // const savedEmail = localStorage.getItem(STORAGE_KEY);
-//   const { email, message } = event.currentTarget.elements;
-//   const formData = {
-//     email: email.value,
-//     message: message.value,
-//     };
-    
-    formData[event.target.name] = event.target.value,
-    
-    console.log(formData);
-    // if (event.value.trim() === '') {
-    //   alert('Всі поля повинні бути заповнені!');
-    // }
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  if (
+    email.value.trim() === '' ||
+    message.value.trim() === ''
+  ) {
+    alert('Всі поля повинні бути заповнені!');
+    return;
+  }
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
-};
+}
 
 function onFormInput(event) {
-  const messageJSON = JSON.stringify(formData);
+  event.preventDefault();
 
-  localStorage.setItem(STORAGE_KEY, messageJSON);
+  const formData = {
+    email: refs.input.value,
+    message: refs.textarea.value,
+  };
+  const formJSON = JSON.stringify(formData);
+
+  localStorage.setItem(STORAGE_KEY, formJSON);
 }
 
 function populateForm() {
-  const savedMessageJSON = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  const savedFormJSON = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-  if (savedMessageJSON) {
-    console.log(savedMessageJSON);
-    // refs.textarea.value = savedMessage;
+  if (savedFormJSON) {
+    console.log(savedFormJSON);
+    refs.input.value = savedFormJSON.email;
+    refs.textarea.value = savedFormJSON.message;
   }
 }
